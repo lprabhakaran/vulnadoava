c9b0571a4714e724fe26baf9f6f98abc7b447bfb
Lekha Prabhakaran <168733991+lprabhakaran@users.noreply.github.com>
2024-06-10 13:51:20 +0530

    Update VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VulnadoApplicationTests {
-	var  aws_access_key_id="AKIAIO5FODNN7EXAMPLE";
+	String  aws_access_key_id = "AKIAIO5FODNN7EXAMPLE";
 
 	@Test
 	public void contextLoads() {

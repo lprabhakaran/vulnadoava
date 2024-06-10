e958f3e51b25f0a2eda57671121847cd9ddfcb62
Lekha Prabhakaran <168733991+lprabhakaran@users.noreply.github.com>
2024-06-10 17:27:56 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    var ghp = "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";
+    String ghp = "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";
     @RequestMapping(value = "/cowsay")
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
